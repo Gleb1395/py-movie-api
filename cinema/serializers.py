8d46cec860901b1b6c33a1fd14dca772a05d7d7d
Глеб Дубинин <gleb1395@gmail.com>
2025-02-05 22:08:40 +0200

    solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,7 +5,7 @@ from cinema.models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=100, required=True)
-    description = serializers.CharField
+    description = serializers.CharField()
     duration = serializers.IntegerField()
 
     class Meta:
